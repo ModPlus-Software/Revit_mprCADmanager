2482a714ba29fb6b48d2abb225f8776c883b77a8
Modis <cjmodis@gmail.com>
2020-09-14 22:01:48 +0300

    Удаление поддержки Revit 2015-2016

--- mprCADmanager/Properties/AssemblyInfo.cs
+++ mprCADmanager/Properties/AssemblyInfo.cs
@@ -12,11 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.2.6.0")]
 [assembly: AssemblyFileVersion("1.2.6.0")]
 
-#if R2015
-[assembly: AssemblyDescription("2015")]
-#elif R2016
-[assembly: AssemblyDescription("2016")]
-#elif R2017
+#if R2017
 [assembly: AssemblyDescription("2017")]
 #elif R2018
 [assembly: AssemblyDescription("2018")]
